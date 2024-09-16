a9474e30fb7c079c48b9cdaddc93b2c7ddddb733
jagotranaveen <jagotra.naveen@gmail.com>
2024-09-16 21:51:20 +0530

    Commit

--- src/api-routes.js
+++ src/api-routes.js
@@ -16,8 +16,8 @@ export const SettingsApiRoutes = {
 };
 
 export const MyEntitiesDashboardApiRoutes = {
-  COUNTRIES: "/entity/myentities/countries",
+  COUNTRIES: "/api/entity/myentities/countries",
   ALL_ENTITIES: "/api/entity/fetchentities?code=LJmz-ek_UtgUoPP2HeHul4ffcOaax8Yn6jrRkbqGR2DIAzFug4106Q%3D%3D",
-  DOWNLOAD_REPORT: "/entity/myentities/download_report",
-  LAST_MONTH_REPORT: "/entity/myentities/lastmonthReport",
+  DOWNLOAD_REPORT: "/api/entity/myentities/download_report",
+  LAST_MONTH_REPORT: "/api/entity/myentities/lastmonthReport",
 };
